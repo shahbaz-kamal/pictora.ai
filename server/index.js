7cd4ebb13ba53ba01c04a11409aab6c49b6f0de4
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-10 00:02:08 +0600

    tested how ai works

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 const port = 5000;
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", () => {
+// google constants
+const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
+// testing ai
+app.get("/test-ai", async (req, res) => {
+  const prompt = "Explain how ai works";
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+  res.send({answer:result.response.text()})
+});
+
+app.get("/", (req, res) => {
   res.send("pictora.ai server is running");
 });
 
